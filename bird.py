93081d0a14de5b8b6e9c51c70135243682acb995
Philipp Nothvogel <phil-n@outlook.com>
2018-04-17 21:22:42 +0200

    add: bird class, falling physics.

---
+++ bird.py
@@ -0,0 +1,37 @@
+import arcade
+from settings import WINDOW_HEIGHT
+from math import sqrt
+
+class Bird:
+    def __init__(self):
+        self.changeY = -1.12
+        self.velocity = 0
+        self.centerY = WINDOW_HEIGHT // 2
+        self.score = 0
+        self.frameCounter = 0
+
+    def draw(self):
+        """ Draw the bird """
+        arcade.draw_circle_filled(40, self.centerY, 18, arcade.color.AUBURN)
+
+    def update(self):
+        """ Code to control the bird's movement. """
+        self.velocity += self.changeY
+        self.centerY += self.velocity
+        self.frameCounter += 1
+        if (self.frameCounter % 60 == 0): 
+            self.score += 1
+            self.frameCounter = 0
+    
+    def up(self):
+        """ Let the bird go up with it's wings """
+        vel = self.velocity
+        change = 0
+        if (vel >= 0):
+            change = 16 - sqrt(vel*12)
+            gravity = abs(self.changeY)
+            if (change < gravity):
+                change = 16
+        else:
+            change = 20 + sqrt(abs(vel))
+        self.velocity += change
